cd40b2ff9168ef7a5290ccda4ae644214a55fee1
Stanislaw Woronowicz <stanislaw.woronowicz@student.pwsz.pl>
2013-10-05 20:42:54 +0200

    inicialize and .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sklep001::Application.config.secret_key_base = '69f8bdb086f2bd0522042a8bcf02f0e74812eb8f92615c9bc733294513c57841523db81b5fae820dae56cfb5bfb51156d5abd4e33f77b5983bdb6f34a7d53438'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sklep001::Application.config.session_store :cookie_store, key: '_sklep001_session'
